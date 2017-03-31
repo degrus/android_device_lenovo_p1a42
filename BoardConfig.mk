cd0a6c45540951599128ff687c0750fdc56a90ef
Bassem Karray <bkarray@gmail.com>
2017-03-31 17:45:50 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,7 +60,8 @@ BOARD_CHARGER_SHOW_PERCENTAGE := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BACKLIGHT_PATH := "/sys/class/leds/button-backlight/brightness"
 
-
+# CMHW
+BOARD_HARDWARE_CLASS := device/lenovo/p1a42/cmhw/src
 
 # FM
 AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
